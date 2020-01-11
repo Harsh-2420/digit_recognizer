07881c1435fd03b20d827f46297f6dc7f07a7615
Harsh <harsh2420@gmail.com>
2020-01-10 20:20:08 -0500

    finished implementing the neural networks and tested on the test set

--- main.py
+++ main.py
@@ -2,25 +2,41 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib
 import matplotlib.pyplot as plt
+import os
+import numpy as np
 
 mnist = keras.datasets.mnist
 
 # Creating the training sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# print(x_train[0])
+
 # Displaying the images
-plt.imshow(x_train[35], cmap="gray")
-plt.show()
+# plt.imshow(x_train[35], cmap="gray")
+# plt.show()
 
-x_train /= 255
-x_test /= 255
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = keras.Sequential([
-    keras.layers.Flatten((28,28)),
-    keras.layers.Dense(128, activation="relu"),
-    keras.layers.Dense(10, activation="softmax")
-])
+# # one-hot coding
+# n_classes = 10
+# Y_train = np_utils.to_categorical(y_train, n_classes)  # What does this do? Read docs
+# Y_test = np_utils.to_categorical(y_test, n_classes)
 
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 model.fit(x_train, y_train, epochs=5)
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("Tested loss:", test_loss)
+print("Tested Acc: ", test_acc)
+
+model.save("digit_recognizer.model")
